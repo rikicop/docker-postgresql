cfd116620760459acb3f24d85a69f6b7f0601c86
Ricardo Otálora Luna <ruperto1@protonmail.com>
2023-02-06 10:36:36 -0500

    alter table students

--- server/student.sql
+++ server/student.sql
@@ -1,2 +1,2 @@
-INSERT INTO students (stu_name, grade, enrollment_date) VALUES ('Martin Rosero', 2, '2023-1-17');
+INSERT INTO students (stu_name, grade, enrollment_date) VALUES ('Martin Rosero', 2, '2023-1-17','n');
 INSERT INTO students (stu_name, grade, enrollment_date) VALUES ('Lucía Albarracín', 2, '2023-1-17');
